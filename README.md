f5dd80caf12e096c9eab0d6314e87ee9ab2a4922
YoRcH <the.yorch@gmail.com>
2016-05-09 14:11:40 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,30 @@
+# WebOgr #
+
+## Description ##
+Web Tool for import and export ESRI Shapefiles.
+
+## Requirements ##
+* [Java](https://www.java.com/es/download/)
+* [Spark](http://www.sparkjava.com/)
+* [ogr2ogr](http://www.gdal.org/ogr2ogr.html)
+
+## Developer Documentation ##
+Execute mvn javadoc:javadoc.
+
+## Installation ##
+Generate and execute jar.
+
+## Notes ##
+This application is a wrapper of ogr2ogr.
+
+## References ##
+http://www.gdal.org/ogr2ogr.html
+
+P.D. Let's go play !!!
+
+
+
+
+
+
+
---
+++ src/main/java/net/yorch/App.java
@@ -0,0 +1,34 @@
+package net.yorch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Web OGR Tool !!!" );
+        
+        
+        String command = "ogr2ogr -f \"ESRI Shapefile\" \"C:/Code/Construcciones.shp\" \"MSSQL:server=148.237.75.92,1433;database=SGC_PRINCIPAL;uid=sa;pwd=password;\" -sql \"SELECT * FROM [dbo].[Construcciones]\" -overwrite -a_srs EPSG:32614";
+        
+        String[] aCommand; 
+		
+		if (System.getProperty("os.name").contains("Windows"))
+			aCommand = new String[]{"cmd.exe","/c",command};
+		else
+			aCommand = new String[]{"/bin/bash","-c",command};
+		
+		int status = 1;
+		
+		try {
+			Process ogr = Runtime.getRuntime().exec(aCommand);
+			status = ogr.waitFor();
+			
+			System.out.println("Status:" + String.valueOf(status));
+			
+			System.out.println("Return Value:" + String.valueOf(ogr.exitValue()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}

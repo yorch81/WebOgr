530aca61934c7c0e0502cb81840f3c90922617e5
YoRcH <the.yorch@gmail.com>
2016-07-19 11:39:26 -0500

    uat

--- src/main/java/net/yorch/OgrApp.java
+++ src/main/java/net/yorch/OgrApp.java
@@ -5,7 +5,7 @@ public class OgrApp {
 	public static void main(String[] args) {
 		System.out.println( "Web OGR Tool !!!" );
         
-		OgrConnection sql = new OgrConnection(OgrConnection.MSSQLSpatial, "localhost", "sa", "", "GEO", 1433);
+		OgrConnection sql = new OgrConnection(OgrConnection.MSSQLSpatial, "localhost", "sa", "password", "SGC_PRINCIPAL", 1433);
 		//OgrConnection mysql = new OgrConnection(OgrConnection.MySQL, "localhost", "root", "", "markers", 3306);
 		
 		if (sql.checkConnection()) {
@@ -13,8 +13,8 @@ public class OgrApp {
 						
 			System.out.println(sql.getOgrTables());
 			
-			//ogr.exportFromDb(sql, "dbo.Predios", "C:/Code/Predios.shp", "EPSG:32614", "EPSG:32614");
-			//ogr.importToDb(mysql, "Construcciones2", "C:/Code/Construcciones.shp", "EPSG:32614", "EPSG:32614");
+			ogr.exportFromDb(sql, "dbo.construcciones", "C:/Code/shapes/construcciones.shp", "EPSG:32614", "EPSG:32614");
+			//ogr.importToDb(sql, "postes_victoria", "C:/Code/shapes/Postes_CdVictoria.shp", "EPSG:4326", "EPSG:4326");
 		}
 		else
 			System.out.println("Not Connected");

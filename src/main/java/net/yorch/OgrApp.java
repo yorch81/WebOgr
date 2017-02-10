3d4757036f0d894d0a1e7470863fabf5b703d9e4
YoRcH <the.yorch@gmail.com>
2017-02-10 12:27:08 -0600

    Changes

--- src/main/java/net/yorch/OgrApp.java
+++ src/main/java/net/yorch/OgrApp.java
@@ -5,20 +5,20 @@ public class OgrApp {
 	public static void main(String[] args) {
 		System.out.println( "Web OGR Tool !!!" );
         
-		//OgrConnection sql = new OgrConnection(OgrConnection.MSSQLSpatial, "Preproduccion", "sa", "", "SGC_PRINCIPAL", 1433);
-		OgrConnection sql = new OgrConnection(OgrConnection.MSSQLSpatial, "localhost", "sa", "", "SGC_PRINCIPAL", 1433);
+		OgrConnection sql = new OgrConnection(OgrConnection.MSSQLSpatial, "IICSRVPRUEBAS", "sa", "u4tpa55w0rd", "SGC_CARTO", 1433);
+		//OgrConnection sql = new OgrConnection(OgrConnection.MSSQLSpatial, "192.168.9.76", "sa", "ser3.PrmT", "SGC_PRINCIPAL", 1433);
 		//OgrConnection sql = new OgrConnection(OgrConnection.PostGis, "localhost", "postgres", "", "postgis_23_sample", 5432);
-		//OgrConnection sql = new OgrConnection(OgrConnection.MySQL, "localhost", "root", "", "GEO", 3306);
+		//OgrConnection sql = new OgrConnection(OgrConnection.MySQL, "localhost", "root", "r00tmysql", "GEO", 3306);
 		
 		if (sql.checkConnection()) {
 			WOgr ogr =  new WOgr();
 						
-			System.out.println(sql.getOgrTables());
+			//System.out.println(sql.getOgrTables());
 			
 			//ogr.importToDb(sql, "predios_vic", "D:/Shapes/SHP_Guemez_2D/Predios_2d.shp", "EPSG:32614", "EPSG:32614");
-			//ogr.importToDb(sql, "const_guemez", "D:/Shapes/SHP_Guemez_2D/Construcciones_2d.shp", "EPSG:32614", "EPSG:32614");
-			//ogr.importToDb(sql, "mzns_guemez", "D:/Shapes/SHP_Guemez_2D/Manzanas_2d.shp", "EPSG:32614", "EPSG:32614");
-			ogr.exportFromDb(sql, "predios_vic", "D:/Shapes/predios_all.shp", "EPSG:32614", "EPSG:32614");
+			//ogr.importToDb(sql, "gz_sector", "C:/CODE/shapes/Manzanas_2d.shp", "EPSG:32614", "EPSG:32614");
+			//ogr.importToDb(sql, "pred_tula", "C:/CODE/shapes/2D/PREDIOS_TULA.shp", "EPSG:32614", "EPSG:32614");
+			ogr.exportFromDb(sql, "gz_sector", "C:/shapes/gz_sector.shp", "EPSG:32614", "EPSG:32614");
 		}
 		else
 			System.out.println("Not Connected");
